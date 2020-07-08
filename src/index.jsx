1c84cb33ab55b42ae2b31f83086473f9929afb10
nawan44 <rachmat.d.gunawan@gmail.com>
2020-07-08 14:27:52 +0700

    basic

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h3>Hello World</h3>
+    </React.StrictMode>, document.getElementById('root')
+)
